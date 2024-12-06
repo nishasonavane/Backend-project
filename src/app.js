f19c881bcd1e957be2a50be3a4658e8b4992952f
Nisha <nishasonavane0958@gmail.com>
2024-12-06 21:55:56 +0530

    router and controller

--- src/app.js
+++ src/app.js
@@ -10,7 +10,16 @@ import cookieParser from "cookie-parser";
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser)
+app.use(cookieParser())
 
+
+
+ //routes import
+
+ import userRouter from "./routes/user.routes.js"
+
+ //routes declaration
+ app.use("/api/v1/users",userRouter)
 
  export {app}
+
